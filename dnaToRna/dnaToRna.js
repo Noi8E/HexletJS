19e6750755aa860edbcac553609edbd24579b4e6
Fedor <noi8e@ya.ru>
2020-02-25 11:16:29 +0300

    начал работу с примером

---
+++ dnaToRna/dnaToRna.js
@@ -0,0 +1,16 @@
+// Четыре нуклеотида в ДНК это аденин (A), цитозин (C), гуанин (G) и тимин (T).
+// Четыре нуклеотида в РНК это аденин (A), цитозин (C), гуанин (G) и урацил (U).
+
+// const dna = "ACGT";
+// const rna = "ACGU";
+
+const adenine = "A";
+const citozine ="C";
+const guanine = "G";
+const timine = "T";
+const urazile ="U";
+const
+
+export default const dnaToRna = (str) => {
+    if (str = null || str = '')
+}
